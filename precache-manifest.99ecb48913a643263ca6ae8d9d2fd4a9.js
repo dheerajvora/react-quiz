82e792ce684d7cd911486103a62ed9d74b8743fa
dheerajvora <dheerajvora1998@gmail.com>
2020-05-01 04:29:37 -1200

    Updates

---
+++ precache-manifest.99ecb48913a643263ca6ae8d9d2fd4a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a67d823095845e84caf62835491c9f0",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "0c6bcc880af4ccbd0de4",
+    "url": "/react-quiz/static/css/main.7907e22d.chunk.css"
+  },
+  {
+    "revision": "c1ef321aa1e31cf6d185",
+    "url": "/react-quiz/static/js/2.f7e15e2d.chunk.js"
+  },
+  {
+    "revision": "0c6bcc880af4ccbd0de4",
+    "url": "/react-quiz/static/js/main.b25ac9e0.chunk.js"
+  },
+  {
+    "revision": "bcdf6f92c64dbd974614",
+    "url": "/react-quiz/static/js/runtime-main.04e0d4a1.js"
+  }
+]);
